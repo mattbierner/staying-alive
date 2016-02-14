99840334a799f3fdd4e4aff3b43c3500a97b27bb
Matt Bierner <mattbierner@gmail.com>
2016-02-14 13:24:45 -0800

    Add cors to server

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const workers = {}
 // start server
 const app = express();
 app.use(require('body-parser').json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.post('/data', (req, res) => {
     const q = req.body;
